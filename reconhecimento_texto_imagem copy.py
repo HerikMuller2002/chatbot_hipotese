ede0e00fa8f24a1b8eebc8db11a76e854d395fa5
Herik <herik.reis02@gmail.com>
2023-05-04 16:00:00 -0300

    preparando correção ortográfica

--- reconhecimento_texto_imagem copy.py
+++
@@ -1,6 +0,0 @@
-import tabula
-
-# Extrair tabelas de uma imagem
-df = tabula.read_pdf('output.pdf', pages='all')[0]
-# salva a tabela como um arquivo Excel
-df.to_excel("arquivo.xlsx", index=False)
